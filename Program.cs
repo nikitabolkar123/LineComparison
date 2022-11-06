7e6a4f0e764ca5392e6e3d38ad522dbabc90dcd0
nikitabolkar123 <bolkarnikita2001@gmail.com>
2022-11-06 07:46:17 -0800

    UC2 added of LineComparison

--- Program.cs
+++ Program.cs
@@ -1,26 +1,48 @@
 ﻿using System;
 
-namespace Uc1_LineComparisons
+namespace UC2_CheckEquality
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Line Comparison Computation Program");
+            Console.WriteLine("Welcome To The Line Comparsion Problems");
 
-            Console.WriteLine("Enter the all Points");
-            int x1 = Convert.ToInt32(Console.ReadLine());
-            int x2 = Convert.ToInt32(Console.ReadLine());
-            int y1 = Convert.ToInt32(Console.ReadLine());
-            int y2 = Convert.ToInt32(Console.ReadLine());
+            int x1, x2, y1, y2;
+            Console.WriteLine("Please Enter X1:");
+            x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please Enter X2:");
+            x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please Enter Y1:");
+            y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please Enter Y2:");
+            y2 = Convert.ToInt32(Console.ReadLine());
 
-            double l1 = Math.Pow((x2 - x1), 2);
-            double l2 = Math.Pow((y2 - y1), 2);
-            double length = l1 + l2;
-            double lengthOfLine = Math.Sqrt(length);
+            double LineLength1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine($"Length of Line 1 is : {LineLength1}");
 
-            Console.WriteLine("Lenth Of The Line is:" + lengthOfLine);
+            int l1, l2, m1, m2;
 
+            Console.WriteLine("Please Enter l1:");
+            l1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please Enter l2:");
+            l2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please Enter m1:");
+            m1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please Enter m2:");
+            m2 = Convert.ToInt32(Console.ReadLine());
+
+            double LineLength2 = Math.Sqrt(Math.Pow(l2 - l1, 2) + Math.Pow(m2 - m1, 2));
+            Console.WriteLine($"Length of the Line 2 is : {LineLength2}");
+
+            if (int.Equals(LineLength1, LineLength2))
+            {
+                Console.WriteLine("Length Of the Two Lines are Equal");
+            }
+            else
+            {
+                Console.WriteLine("Length Of the Two Lines are Not Equal");
+            }
 
         }
     }
